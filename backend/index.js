138871db3e06706ab62fcacdd8e5bcaa8fb87b1e
Ilyas Assakali <ilyas.assakali@student.ehb.be>
2022-12-07 17:19:53 +0100

    loginsystempart2

--- backend/index.js
+++ backend/index.js
@@ -35,5 +35,44 @@ app.post("/register", (req, res) => {
 
 })
 
+app.post("/login", (req, res) => {
+
+
+    //check for empty fields
+    if (!req.body.email || !req.body.password) {
+        res.status(401).send({
+            status: "Bad request",
+            message: "Some fields are missing: username, email, password"
+        })
+    }
+
+    //check for the user in array users
+    let user = users.find(element => element.email == req.body.email)
+
+    if (user) {
+        //compare passwords
+        if (user.password == req.body.password) {
+            //password is correct
+            res.status(200).send({
+                status: "Authentication succesfull!",
+                message: "You are logged in!"
+            })
+        } else {
+            //password is incorrect
+            res.status(401).send({
+                status: "Authentication error",
+                message: "Password is incorrect!"
+            })
+        }
+    } else {
+        //no user found: send back error
+        res.status(401).send({
+            status: "Authentication error",
+            message: "No user with email has been found! Make sure your register first!"
+        })
+    }
+})
+
+
 app.listen(3000);
 console.log("app running at http://localhost:3000");
