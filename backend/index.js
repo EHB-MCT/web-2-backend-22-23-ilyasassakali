b4ecae61512989ff004e45f15462fd939e28435b
Ilyas Assakali <ilyas.assakali@student.ehb.be>
2022-12-18 23:37:59 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,11 +15,16 @@ const Artist = require('./modal/Artist.js')
 //create mongoclient
 const client = new MongoClient(process.env.FINAL_URL)
 
+const port = process.env.port || 1337;
+
 app.use(express.urlencoded({
     extended: false
 }));
 app.use(cors())
 app.use(express.json())
+
+
+
 
 
 //CRUD MUZZY SYSTEM
